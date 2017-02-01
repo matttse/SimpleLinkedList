c0c0625d581c5ad7c59da1f26c9164e25d408797
Matt Tse <tsemd@bu.edu>
2017-02-01 09:36:41 -0500

    initial commit

---
+++ src/StudentCollection.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @ClassName StudentCollection
+ * @author Matthew Tse
+ *
+ */
+public class StudentCollection {
+	//keeps identification numbers of students
+	private String[] ids;
+	//keeps the names of students
+	private String[] names;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
